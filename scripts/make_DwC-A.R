545479a4aba774592caec337dcaea2737ae6c68a
Matthew L. Bowser <matt_bowser@fws.gov>
2019-12-23 09:14:14 -0900

    Update data and scripts
    -The fields TaxonKingdom, TaxonPhylum, TaxonClass were added to the
    FWSpecies report. Data and a script are updated to reflect this.
    -The species Quedionuchus plagiatus is moved to FWSpecies.

--- scripts/make_DwC-A.R
+++ scripts/make_DwC-A.R
@@ -1,11 +1,10 @@
 ## Work toward generating Darwin Core Archives from FWSpecies checklists.
-## Load functions.
+## Load functions and libraries.
 source("functions.R")
 library("zip")
 
 ## First load data.
 data1 <- assemble_csvs(directory="../data/FWSpecies")
-categories_kingdoms <- read.csv("../data/CategoryGroup_kingdom_crosswalk.csv", colClasses="character")
 fields_crosswalk <- read.csv("../data/field_name_crosswalk.csv", colClasses="character")
 
 ## Renaming fields.
@@ -15,20 +14,10 @@ for (this_field in 1:nrow(fields_crosswalk))
  names(data1)[sl] <- fields_crosswalk$DwC_field[this_field]
  }
 
-## Add kingdoms
-data2 <- merge(
- x=data1,
- y=categories_kingdoms,
- by="TaxonCategoryGroup",
- all.x=TRUE
- )
- 
-## Phyla and classes are still missing.
-
 ## Trying to make a minimal DwC taxon.txt file.
-data2 <- data2[order(data2$kingdom, data2$scientificName),]
-data2$ID <- data2$taxonID
-dwc1 <- data2[,c("ID", "taxonID", "scientificName", "taxonRank", "kingdom")]
+data1 <- data1[order(data1$kingdom, data1$scientificName),]
+data1$ID <- data1$taxonID
+dwc1 <- data1[,c("ID", "taxonID", "scientificName", "taxonRank", "kingdom")]
 write.table(
  dwc1,
  file="../data/DwC-A/taxon.txt",
