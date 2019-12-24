9efaa69c0b530eede6013658f9a6da9106307b13
Matthew L. Bowser <matt_bowser@fws.gov>
2019-12-23 15:54:51 -0900

    Improve DwC-A output

--- scripts/make_DwC-A.R
+++ scripts/make_DwC-A.R
@@ -1,4 +1,14 @@
 ## Work toward generating Darwin Core Archives from FWSpecies checklists.
+
+## Guidance:
+## https://github.com/gbif/ipt/wiki/DwCAHowToGuide
+## https://github.com/gbif/ipt/wiki/checklistData
+
+## Tools:
+## http://tools.gbif.org/dwca-assistant/
+## https://tools.gbif.org/dwca-validator/
+## https://www.gbif.org/tools/data-validator
+
 ## Load functions and libraries.
 source("functions.R")
 library("zip")
@@ -13,11 +23,30 @@ for (this_field in 1:nrow(fields_crosswalk))
  sl <- which(names(data1) == fields_crosswalk$FWSpecies_field[this_field])
  names(data1)[sl] <- fields_crosswalk$DwC_field[this_field]
  }
-
+ 
 ## Trying to make a minimal DwC taxon.txt file.
-data1 <- data1[order(data1$kingdom, data1$scientificName),]
+data1$taxonRank <- tolower(data1$taxonRank)
+data1 <- data1[order(
+ data1$kingdom,
+ data1$phylum,
+ data1$class,
+ data1$order,
+ data1$family,
+ data1$scientificName
+ ),]
+data1 <- data1[data1$taxonRank == "species",] ## Limiting the list to species only for now.
 data1$ID <- data1$taxonID
-dwc1 <- data1[,c("ID", "taxonID", "scientificName", "taxonRank", "kingdom")]
+dwc1 <- data1[,c(
+ "ID",
+ "taxonID",
+ "kingdom",
+ "phylum",
+ "class",
+ "order",
+ "family",
+ "scientificName",
+ "taxonRank"
+ )]
 write.table(
  dwc1,
  file="../data/DwC-A/taxon.txt",
@@ -25,6 +54,23 @@ write.table(
  sep = "\t",
  row.names=FALSE
  )
+
+## Now generate a meta.xml file. 
+## Header...
+xml_file <- "../data/DwC-A/meta.xml"
+write('<?xml version="1.0"?>
+<archive xmlns="http://rs.tdwg.org/dwc/text/">
+\t<core encoding="UTF-8" linesTerminatedBy="\\r\\n" fieldsTerminatedBy="\\t" fieldsEnclosedBy="" ignoreHeaderLines="1" rowType="http://rs.tdwg.org/dwc/terms/Taxon">
+\t\t<files>
+\t\t\t<location>taxon.txt</location>
+\t\t</files>
+\t\t<id index="0"/>', xml_file, append=FALSE)
+for (this_col in 2:ncol(dwc1))
+ {
+ write(paste0('\t\t<field index="', this_col-1, '" term="http://rs.tdwg.org/dwc/terms/', names(dwc1)[this_col], '"/>'), xml_file, append=TRUE)
+ }
+write('\t</core>
+</archive>', xml_file, append=TRUE) 
  
 zipr(
  zipfile="../data/DwC-A/KenaiNWRspecies_DwC-A.zip",
