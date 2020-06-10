ef10c740d5986cf8584fa8ca00a69260b0290149
Matthew L. Bowser <matt_bowser@fws.gov>
2020-06-09 16:39:23 -0800

    Add indexing for kingdoms through genera

--- scripts/make_checklist_TeX.R
+++ scripts/make_checklist_TeX.R
@@ -32,7 +32,7 @@ print_taxon <- function(outfile,
   name <- "taxon name missing"
   }
  rank <- simpleCap(rank)
- write(paste0("\\vspace{", vspace, "}\\noindent\\hspace{", hspace, "}", rank, " ", name, "\n\n"), file=outfile, append=TRUE)
+ write(paste0("\n\\vspace{", vspace, "}\\noindent\\hspace{", hspace, "}", rank, " ", name), file=outfile, append=TRUE)
  if(!vernacularName=="")
   {
   write(
@@ -74,45 +74,96 @@ gns <- "NA"
 for (this_record in 1:nrow(cl1)) #nrow(cl1)
  {
  
- if (!(kdm == cl1$kingdom[this_record]))
-  {
-  print_taxon(outfile=outfile, name=cl1$kingdom[this_record], rank="Kingdom")
-  kdm <- cl1$kingdom[this_record]
-  }
- 
-  if (!(plm == cl1$phylum[this_record]))
-  {
-  print_taxon(outfile=outfile, name=cl1$phylum[this_record], rank="Phylum",
- hspace="6pt")
-  plm <- cl1$phylum[this_record]
-  }
-  
-  if (!(cls == cl1$class[this_record]))
-  {
-  print_taxon(outfile=outfile, name=cl1$class[this_record], rank="Class",
- hspace="12pt")
-  cls <- cl1$class[this_record]
-  }
- 
-  if (!(odr == cl1$order[this_record]))
-  {
-  print_taxon(outfile=outfile, name=cl1$order[this_record], rank="Order",
- hspace="18pt")
-  odr <- cl1$order[this_record]
-  }
-  
-  if (!(fml == cl1$family[this_record]))
-  {
-  print_taxon(outfile=outfile, name=cl1$family[this_record], rank="Family",
- hspace="24pt")
-  fml <- cl1$family[this_record]
-  }
- 
   if (!(gns == cl1$genus[this_record]))
   {
+  if(!(gns=="NA"))
+   {
+   write(paste0("\\index{", gns, "@\\textit{", gns, "}|)}\n"), file=outfile, append=TRUE)
+   }
+  }
+ 
+ if (!(fml == cl1$family[this_record]))
+  {
+  if(!(fml=="NA"))
+   {
+   write(paste0("\\index{", fml, "|)}\n"), file=outfile, append=TRUE)
+   }
+  }
+ 
+ if (!(odr == cl1$order[this_record]))
+  {
+  if(!(odr=="NA"))
+   {
+   write(paste0("\\index{", odr, "|)}\n"), file=outfile, append=TRUE)
+   }
+  }
+   
+ if (!(cls == cl1$class[this_record]))
+  {
+  if(!(cls=="NA"))
+   {
+   write(paste0("\\index{", cls, "|)}\n"), file=outfile, append=TRUE)
+   }
+  }
+ 
+ if (!(plm == cl1$phylum[this_record]))
+  {
+  if(!(plm=="NA"))
+   {
+   write(paste0("\n\\index{", plm, "|)}\n"), file=outfile, append=TRUE)
+   }
+  }
+ 
+ if (!(kdm == cl1$kingdom[this_record]))
+  {
+  if(!(kdm=="NA"))
+   {
+   write(paste0("\\index{", kdm, "|)}\n"), file=outfile, append=TRUE)
+   }
+  kdm <- cl1$kingdom[this_record] 
+  write(paste0("\n\\index{", kdm, "|(}"), file=outfile, append=TRUE) 
+  print_taxon(outfile=outfile, name=cl1$kingdom[this_record], rank="Kingdom")
+  }
+ 
+ if (!(plm == cl1$phylum[this_record]))
+  {
+  plm <- cl1$phylum[this_record]
+  write(paste0("\n\\index{", plm, "|(}"), file=outfile, append=TRUE) 
+  print_taxon(outfile=outfile, name=cl1$phylum[this_record], rank="Phylum",
+ hspace="6pt")
+  
+  }
+  
+ if (!(cls == cl1$class[this_record]))
+  {
+  cls <- cl1$class[this_record]
+  write(paste0("\n\\index{", cls, "|(}"), file=outfile, append=TRUE)
+  print_taxon(outfile=outfile, name=cl1$class[this_record], rank="Class",
+ hspace="12pt")
+  }
+ 
+ if (!(odr == cl1$order[this_record]))
+  {
+  odr <- cl1$order[this_record]
+  write(paste0("\n\\index{", odr, "|(}"), file=outfile, append=TRUE)
+  print_taxon(outfile=outfile, name=cl1$order[this_record], rank="Order",
+ hspace="18pt")
+  }
+  
+ if (!(fml == cl1$family[this_record]))
+  {
+  fml <- cl1$family[this_record]
+  write(paste0("\n\\index{", fml, "|(}"), file=outfile, append=TRUE)
+  print_taxon(outfile=outfile, name=cl1$family[this_record], rank="Family",
+ hspace="24pt")
+  }
+ 
+ if (!(gns == cl1$genus[this_record]))
+  {
+  gns <- cl1$genus[this_record]
+  write(paste0("\\index{", gns, "@\\textit{", gns, "}|(}\n"), file=outfile, append=TRUE)
   print_taxon(outfile=outfile, name=paste0("\\textit{", cl1$genus[this_record], "}"), rank="Genus",
  hspace="30pt")
-  gns <- cl1$genus[this_record]
   }
  
  print_taxon(
@@ -152,8 +203,15 @@ for (this_record in 1:nrow(cl1)) #nrow(cl1)
    }  
   }
  }
+write(paste0("\\index{", gns, "@\\textit{", gns, "}|)}\n"), file=outfile, append=TRUE)
+write(paste0("\\index{", fml, "|)}"), file=outfile, append=TRUE)
+write(paste0("\\index{", odr, "|)}"), file=outfile, append=TRUE)
+write(paste0("\\index{", cls, "|)}"), file=outfile, append=TRUE)
+write(paste0("\\index{", plm, "|)}"), file=outfile, append=TRUE)
+write(paste0("\\index{", kdm, "|)}"), file=outfile, append=TRUE)
  
-wline <- "\\end{multicols}\n\
+wline <- "\n\\printindex\n
+\\end{multicols}\n\
 \\end{document}"
 write(wline, outfile, append=TRUE)
  
